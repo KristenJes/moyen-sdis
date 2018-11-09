5f8ddcb029947145c3adc938988c8730477f6317
Kristen <KristenJes@users.noreply.github.com>
2018-11-09 10:57:16 +0100

    pompier

--- moyens-sdis/Form1.Designer.vb
+++ moyens-sdis/Form1.Designer.vb
@@ -22,17 +22,30 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.SocGrid = New System.Windows.Forms.DataGridView()
+        CType(Me.SocGrid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'SocGrid
+        '
+        Me.SocGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.SocGrid.Location = New System.Drawing.Point(12, 12)
+        Me.SocGrid.Name = "SocGrid"
+        Me.SocGrid.Size = New System.Drawing.Size(488, 411)
+        Me.SocGrid.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(517, 435)
+        Me.Controls.Add(Me.SocGrid)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.SocGrid, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents SocGrid As System.Windows.Forms.DataGridView
 
 End Class
--- moyens-sdis/Form1.vb
+++ moyens-sdis/Form1.vb
@@ -2,5 +2,11 @@
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim conn As New Connexion("cnx_oraD22", "SDIS29", "Iroise29")
+        Dim table As DataTable = conn.Table("SELECT * FROM POMPIER")
+        'Dim table As DataTable = conn.Table("SELECT * FROM ENGIN")
+
+        SocGrid.DataSource = table
+
     End Sub
+
 End Class
