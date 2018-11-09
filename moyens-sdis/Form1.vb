413ed584ed743eceb08c6ac236461e8cab33eb45
Kristen <KristenJes@users.noreply.github.com>
2018-11-09 10:44:41 +0100

    Edit

--- moyens-sdis/Form1.vb
+++ moyens-sdis/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim conn As New Connexion("cnx_oraD22", "SDIS29", "Iroise29")
+    End Sub
 End Class
