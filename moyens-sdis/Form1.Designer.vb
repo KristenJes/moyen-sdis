6f153a1f5fff100306f5741b3f3c2ee463fc4251
Kristen <INSTALL@estransup.local>
2018-11-16 10:53:01 +0100

    kris

--- moyens-sdis/Form1.Designer.vb
+++ moyens-sdis/Form1.Designer.vb
@@ -23,29 +23,42 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.SocGrid = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.SocGrid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'SocGrid
         '
         Me.SocGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.SocGrid.Location = New System.Drawing.Point(12, 12)
+        Me.SocGrid.Location = New System.Drawing.Point(12, 286)
         Me.SocGrid.Name = "SocGrid"
-        Me.SocGrid.Size = New System.Drawing.Size(488, 411)
+        Me.SocGrid.Size = New System.Drawing.Size(488, 137)
         Me.SocGrid.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(517, 435)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.SocGrid)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.SocGrid, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents SocGrid As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- moyens-sdis/Form1.vb
+++ moyens-sdis/Form1.vb
@@ -3,13 +3,27 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim conn As New Connexion("cnx_oraD22", "SDIS29", "Iroise29")
         Dim table As DataTable = conn.Table("SELECT * FROM POMPIER")
-        'Dim table As DataTable = conn.Table("SELECT * FROM ENGIN")
 
         SocGrid.DataSource = table
+
+        For Each row As DataRow In table.Rows
+            ' Write value of first Integer.
+            Label1.Text = row("SP_NOM")
+            GoTo end_of_for
+        Next
+
+end_of_for:
+
+        Dim s As String = table.Rows(1)("SP_NOM").ToString()
+        'Label1.Text = ss
 
     End Sub
 
     Private Sub SocGrid_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles SocGrid.CellContentClick
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
 
     End Sub
 End Class
