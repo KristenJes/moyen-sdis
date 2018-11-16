70190f3b76b97917fe6f16c88ce49068a9bec751
AlanJcb <ajacob@estran-brest.education>
2018-11-16 11:40:35 +0100

    classe

--- moyens-sdis/Classes/Caserne.vb
+++ moyens-sdis/Classes/Caserne.vb
@@ -6,4 +6,52 @@
     Private _longitude As String
     Private _engins As List(Of Engin)
 
+    Public Property Engins As List(Of Engin)
+        Get
+            Return _engins
+
+        End Get
+        Set(ByVal value As List(Of Engin))
+
+            _engins = value
+        End Set
+    End Property
+
+    Public Property ID As Integer
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Latitude As Integer
+        Get
+            Return _latitude
+        End Get
+        Set(ByVal value As Integer)
+            _latitude = value
+        End Set
+    End Property
+
+    Public Property Longitude As Integer
+        Get
+            Return _longitude
+        End Get
+        Set(ByVal value As Integer)
+            _longitude = value
+        End Set
+    End Property
+
+    Public Property Nom As String
+        Get
+            Return _nom
+        End Get
+        Set(ByVal value As String)
+
+            _nom = value
+        End Set
+    End Property
+
 End Class
--- moyens-sdis/Classes/Disponibilite.vb
+++ moyens-sdis/Classes/Disponibilite.vb
@@ -1,4 +1,22 @@
 ﻿Public Class Disponibilite
-    Private _dispoId
-    Private _dispoLib
+    Private _dispoId As Integer
+    Private _dispoLib As String
+
+    Public Property DispoID As Integer
+        Get
+            Return _dispoId
+        End Get
+        Set(ByVal value As Integer)
+            _dispoId = value
+        End Set
+    End Property
+
+    Public Property DispoLib As String
+        Get
+            Return _dispoLib
+        End Get
+        Set(ByVal value As String)
+            _dispoLib = value
+        End Set
+    End Property
 End Class
