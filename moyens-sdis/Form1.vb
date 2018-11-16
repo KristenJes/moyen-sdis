3862cf3c88cd7cf7254c12c7bff465d844f79d57
unknown <17kjestin@ESTRANSUP.LOCAL>
2018-11-16 09:31:48 +0100

    xcv

--- moyens-sdis/Form1.vb
+++ moyens-sdis/Form1.vb
@@ -3,10 +3,13 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim conn As New Connexion("cnx_oraD22", "SDIS29", "Iroise29")
         Dim table As DataTable = conn.Table("SELECT * FROM POMPIER")
-        Dim table As DataTable = conn.Table("SELECT * FROM ENGIN")
+        'Dim table As DataTable = conn.Table("SELECT * FROM ENGIN")
 
         SocGrid.DataSource = table
 
     End Sub
 
+    Private Sub SocGrid_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles SocGrid.CellContentClick
+
+    End Sub
 End Class
