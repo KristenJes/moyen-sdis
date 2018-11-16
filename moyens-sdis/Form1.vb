484991188d0b294eac644c80ecb5ccd11c84b346
AlanJcb <ajacob@estran-brest.education>
2018-11-16 09:32:08 +0100

    .

--- moyens-sdis/Form1.vb
+++ moyens-sdis/Form1.vb
@@ -3,7 +3,7 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim conn As New Connexion("cnx_oraD22", "SDIS29", "Iroise29")
         Dim table As DataTable = conn.Table("SELECT * FROM POMPIER")
-        Dim table As DataTable = conn.Table("SELECT * FROM ENGIN")
+        Dim table2 As DataTable = conn.Table("SELECT * FROM ENGIN")
 
         SocGrid.DataSource = table
 
