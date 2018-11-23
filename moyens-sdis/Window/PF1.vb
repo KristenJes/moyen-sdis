b6ee54dcc614f124e59af4f9ff087d2d94cc2b19
unknown <17KJESTIN@estransup.local>
2018-11-23 09:20:20 +0100

    re

---
+++ moyens-sdis/Window/PF1.vb
@@ -0,0 +1,21 @@
+﻿Public Class PF1
+
+    Private departs As List(Of Depart)
+
+
+    Private Sub PF1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        For Each depart As Depart In departs
+
+        Next
+
+    End Sub
+
+
+    ' A METTRE DANS LA CLASSE DEPART
+    'Public Function AfficheDepart(ByVal depart As Depart)
+    '    depart()
+
+    '    Return "";
+    'End Function
+End Class
