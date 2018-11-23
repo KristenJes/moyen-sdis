22a870a7ab4c28aede47af765c20c0bb4fcda2d1
melvynly <melvynly@users.noreply.github.com>
2018-11-23 11:44:38 +0100

    yy

--- moyens-sdis/Window/PF5/CRUDENGINS.vb
+++ moyens-sdis/Window/PF5/CRUDENGINS.vb
@@ -6,53 +6,52 @@ Class CRUDENGINS
 
             uneListView.Clear()
 
-        '    uneListView.Columns.Add("Caserne", 80, HorizontalAlignment.Left)
-        '    uneListView.Columns.Add("Nom engin", 70, HorizontalAlignment.Left)
-        '    uneListView.Columns.Add("Imatriculation", 90, HorizontalAlignment.Left)
-        '    uneListView.Columns.Add("Etat", 120, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Caserne", 80, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Nom engin", 70, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Imatriculation", 90, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Etat", 120, HorizontalAlignment.Left)
 
-        '    Dim info(5) As String
-        '    Dim itm As ListViewItem
+        Dim info(5) As String
+        Dim itm As ListViewItem
 
-        '    info(0) = "Ouessant"
-        '    info(1) = "VSAV02"
-        '    info(2) = "XXX-123-YYY"
-        '    info(3) = "OK"
-
-        '    itm = New ListViewItem(info)
-        '    uneListView.Items.Add(itm)
-
-
-        '    info(0) = "Brest"
-        '    info(1) = "FPT04"
-        '    info(2) = "XXX-123-YYY"
-        '    info(3) = "En intervention"
-
-
-        '    itm = New ListViewItem(info)
-        '    uneListView.Items.Add(itm)
-
-        '    info(0) = "Ouessant"
-        '    info(1) = "VTU01"
-        '    info(2) = "XXX-123-YYY"
-        '    info(3) = "OK"
-
-        '    itm = New ListViewItem(info)
-        'uneListView.Items.Add(itm)
+        'info(0) = "Ouessant"
+        'info(1) = "VSAV02"
+        'info(2) = "XXX-123-YYY"
+        'info(3) = "OK"
 
 
 
-        Dim test As DataTable = Connexion.ORA.Table("SELECT * FROM ENGIN")
-        For Each nom As DataRow In test.Rows
-            ''nom("hg")
+
+
+
+        Dim engin_id As DataTable = Connexion.ORA.Table("SELECT ENGIN_ID FROM ENGIN")
+        For Each nom As DataRow In engin_id.Rows
+            info(0) = nom("ENGIN_ID").ToString
         Next
 
 
 
-        For Each nom As DataRow In test.Rows
-            ' Write value of first Integer.
-            Label.nom("Nom")
+        Dim engin_nom As DataTable = Connexion.ORA.Table("SELECT ENGIN_NOM FROM ENGIN")
+        For Each nom As DataRow In engin_nom.Rows
+            info(1) = nom("ENGIN_NOM").ToString
         Next
+
+
+        Dim engin_immat As DataTable = Connexion.ORA.Table("SELECT ENGIN_IMMAT FROM ENGIN")
+        For Each nom As DataRow In engin_immat.Rows
+            info(2) = nom("ENGIN_IMMAT").ToString
+        Next
+
+
+        Dim engin_etat As DataTable = Connexion.ORA.Table("SELECT ENGIN_ETAT FROM ENGIN")
+        For Each nom As DataRow In engin_etat.Rows
+            info(3) = nom("ENGIN_ETAT").ToString
+        Next
+
+        itm = New ListViewItem(info)
+        uneListView.Items.Add(itm)
+
+
 
     End Sub
         Public Sub ConfirmDelete()
