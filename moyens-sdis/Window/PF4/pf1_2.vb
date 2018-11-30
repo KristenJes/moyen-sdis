6e2d4aef579ba1816ac5a4d613a9f9307981439d
Kristen <35566182+KristenJes@users.noreply.github.com>
2018-11-30 08:39:31 +0100

    Add files via upload

---
+++ moyens-sdis/Window/PF4/pf1_2.vb
@@ -0,0 +1,31 @@
+﻿Public Class pf1_2
+
+
+
+    Private Sub pf1_2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ListBox1.Items.Add("BREST")
+        ListBox1.Items.Add("OUESSANT")
+        ListBox1.Items.Add("SAINT RENAN")
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub lblCa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblCa.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub lblNomCa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblNomCa.Click
+
+    End Sub
+
+    Private Sub lblCaserne_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblCaserne.Click
+
+    End Sub
+End Class
