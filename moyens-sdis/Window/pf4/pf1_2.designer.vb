eacd6402eeb7be2a1352a3d69a83ccbffb0df7cf
AlanJcb <ajacob@estran-brest.education>
2018-11-30 08:18:34 +0100

    commit du matin

--- moyens-sdis/Window/pf4/pf1_2.designer.vb
+++ moyens-sdis/Window/pf4/pf1_2.designer.vb
@@ -23,22 +23,11 @@ Partial Class pf1_2
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblCaserne = New System.Windows.Forms.Label()
-        Me.lblCa = New System.Windows.Forms.Label()
-        Me.lblCond = New System.Windows.Forms.Label()
-        Me.lblEquip1 = New System.Windows.Forms.Label()
-        Me.lblEquip2 = New System.Windows.Forms.Label()
-        Me.lblEquip3 = New System.Windows.Forms.Label()
-        Me.lblEquip4 = New System.Windows.Forms.Label()
-        Me.lblNomCa = New System.Windows.Forms.Label()
-        Me.lblNomCond = New System.Windows.Forms.Label()
-        Me.lblNomEquip1 = New System.Windows.Forms.Label()
-        Me.lblNomEquip2 = New System.Windows.Forms.Label()
-        Me.lblNomEquip3 = New System.Windows.Forms.Label()
-        Me.lblNomEquip4 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblCa = New System.Windows.Forms.Label()
+        Me.lblNomCa = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -47,169 +36,12 @@ Partial Class pf1_2
         Me.lblCaserne.BackColor = System.Drawing.SystemColors.ScrollBar
         Me.lblCaserne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.lblCaserne.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCaserne.Location = New System.Drawing.Point(21, 19)
+        Me.lblCaserne.Location = New System.Drawing.Point(77, 19)
         Me.lblCaserne.Name = "lblCaserne"
-        Me.lblCaserne.Size = New System.Drawing.Size(100, 23)
+        Me.lblCaserne.Size = New System.Drawing.Size(150, 23)
         Me.lblCaserne.TabIndex = 0
         Me.lblCaserne.Text = "FTP 01"
         Me.lblCaserne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblCa
-        '
-        Me.lblCa.BackColor = System.Drawing.SystemColors.Window
-        Me.lblCa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblCa.Location = New System.Drawing.Point(15, 15)
-        Me.lblCa.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblCa.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblCa.Name = "lblCa"
-        Me.lblCa.Size = New System.Drawing.Size(80, 18)
-        Me.lblCa.TabIndex = 1
-        Me.lblCa.Text = "CA INC"
-        Me.lblCa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblCond
-        '
-        Me.lblCond.BackColor = System.Drawing.SystemColors.Window
-        Me.lblCond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblCond.Location = New System.Drawing.Point(15, 45)
-        Me.lblCond.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblCond.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblCond.Name = "lblCond"
-        Me.lblCond.Size = New System.Drawing.Size(80, 18)
-        Me.lblCond.TabIndex = 2
-        Me.lblCond.Text = "COND INC"
-        Me.lblCond.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblEquip1
-        '
-        Me.lblEquip1.BackColor = System.Drawing.SystemColors.Window
-        Me.lblEquip1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblEquip1.Location = New System.Drawing.Point(15, 75)
-        Me.lblEquip1.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip1.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip1.Name = "lblEquip1"
-        Me.lblEquip1.Size = New System.Drawing.Size(80, 18)
-        Me.lblEquip1.TabIndex = 3
-        Me.lblEquip1.Text = "EQUIP INC"
-        Me.lblEquip1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblEquip2
-        '
-        Me.lblEquip2.AutoSize = True
-        Me.lblEquip2.BackColor = System.Drawing.SystemColors.Window
-        Me.lblEquip2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblEquip2.Location = New System.Drawing.Point(15, 105)
-        Me.lblEquip2.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip2.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip2.Name = "lblEquip2"
-        Me.lblEquip2.Size = New System.Drawing.Size(80, 18)
-        Me.lblEquip2.TabIndex = 4
-        Me.lblEquip2.Text = "EQUIP INC"
-        Me.lblEquip2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblEquip3
-        '
-        Me.lblEquip3.AutoSize = True
-        Me.lblEquip3.BackColor = System.Drawing.SystemColors.Window
-        Me.lblEquip3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblEquip3.Location = New System.Drawing.Point(15, 135)
-        Me.lblEquip3.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip3.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip3.Name = "lblEquip3"
-        Me.lblEquip3.Size = New System.Drawing.Size(80, 18)
-        Me.lblEquip3.TabIndex = 5
-        Me.lblEquip3.Text = "EQUIP INC"
-        Me.lblEquip3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblEquip4
-        '
-        Me.lblEquip4.AutoSize = True
-        Me.lblEquip4.BackColor = System.Drawing.SystemColors.Window
-        Me.lblEquip4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblEquip4.Location = New System.Drawing.Point(15, 165)
-        Me.lblEquip4.MaximumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip4.MinimumSize = New System.Drawing.Size(80, 18)
-        Me.lblEquip4.Name = "lblEquip4"
-        Me.lblEquip4.Size = New System.Drawing.Size(80, 18)
-        Me.lblEquip4.TabIndex = 6
-        Me.lblEquip4.Text = "EQUIP INC"
-        Me.lblEquip4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomCa
-        '
-        Me.lblNomCa.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomCa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomCa.Location = New System.Drawing.Point(124, 16)
-        Me.lblNomCa.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomCa.Name = "lblNomCa"
-        Me.lblNomCa.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomCa.TabIndex = 7
-        Me.lblNomCa.Text = "BUFFFERTRILLE MICHEL"
-        Me.lblNomCa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomCond
-        '
-        Me.lblNomCond.AutoSize = True
-        Me.lblNomCond.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomCond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomCond.Location = New System.Drawing.Point(124, 45)
-        Me.lblNomCond.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomCond.Name = "lblNomCond"
-        Me.lblNomCond.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomCond.TabIndex = 8
-        Me.lblNomCond.Text = "LETANO SEBASTIEN"
-        Me.lblNomCond.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomEquip1
-        '
-        Me.lblNomEquip1.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomEquip1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomEquip1.Location = New System.Drawing.Point(124, 75)
-        Me.lblNomEquip1.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip1.Name = "lblNomEquip1"
-        Me.lblNomEquip1.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip1.TabIndex = 9
-        Me.lblNomEquip1.Text = "PERUE DAMIEN"
-        Me.lblNomEquip1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomEquip2
-        '
-        Me.lblNomEquip2.AutoSize = True
-        Me.lblNomEquip2.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomEquip2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomEquip2.Location = New System.Drawing.Point(124, 105)
-        Me.lblNomEquip2.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip2.Name = "lblNomEquip2"
-        Me.lblNomEquip2.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip2.TabIndex = 10
-        Me.lblNomEquip2.Text = "GRETHEN SEVERINE"
-        Me.lblNomEquip2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomEquip3
-        '
-        Me.lblNomEquip3.AutoSize = True
-        Me.lblNomEquip3.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomEquip3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomEquip3.Location = New System.Drawing.Point(124, 135)
-        Me.lblNomEquip3.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip3.Name = "lblNomEquip3"
-        Me.lblNomEquip3.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip3.TabIndex = 11
-        Me.lblNomEquip3.Text = "DUBOIS MARTIAL"
-        Me.lblNomEquip3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'lblNomEquip4
-        '
-        Me.lblNomEquip4.AutoSize = True
-        Me.lblNomEquip4.BackColor = System.Drawing.SystemColors.Window
-        Me.lblNomEquip4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblNomEquip4.Location = New System.Drawing.Point(124, 165)
-        Me.lblNomEquip4.MinimumSize = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip4.Name = "lblNomEquip4"
-        Me.lblNomEquip4.Size = New System.Drawing.Size(149, 18)
-        Me.lblNomEquip4.TabIndex = 12
-        Me.lblNomEquip4.Text = "SWELLAM KARIM"
-        Me.lblNomEquip4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Button1
         '
@@ -229,19 +61,34 @@ Partial Class pf1_2
         Me.Button2.Text = "Annuler"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'lblCa
+        '
+        Me.lblCa.BackColor = System.Drawing.SystemColors.Window
+        Me.lblCa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCa.Location = New System.Drawing.Point(15, 15)
+        Me.lblCa.MaximumSize = New System.Drawing.Size(80, 18)
+        Me.lblCa.MinimumSize = New System.Drawing.Size(80, 18)
+        Me.lblCa.Name = "lblCa"
+        Me.lblCa.Size = New System.Drawing.Size(80, 18)
+        Me.lblCa.TabIndex = 1
+        Me.lblCa.Text = " "
+        Me.lblCa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNomCa
+        '
+        Me.lblNomCa.BackColor = System.Drawing.SystemColors.Window
+        Me.lblNomCa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNomCa.Location = New System.Drawing.Point(141, 15)
+        Me.lblNomCa.MinimumSize = New System.Drawing.Size(149, 18)
+        Me.lblNomCa.Name = "lblNomCa"
+        Me.lblNomCa.Size = New System.Drawing.Size(149, 18)
+        Me.lblNomCa.TabIndex = 7
+        Me.lblNomCa.Text = "BUFFFERTRILLE MICHEL"
+        Me.lblNomCa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.lblNomEquip4)
-        Me.GroupBox1.Controls.Add(Me.lblNomEquip3)
-        Me.GroupBox1.Controls.Add(Me.lblNomEquip2)
-        Me.GroupBox1.Controls.Add(Me.lblNomEquip1)
-        Me.GroupBox1.Controls.Add(Me.lblNomCond)
         Me.GroupBox1.Controls.Add(Me.lblNomCa)
-        Me.GroupBox1.Controls.Add(Me.lblEquip4)
-        Me.GroupBox1.Controls.Add(Me.lblEquip3)
-        Me.GroupBox1.Controls.Add(Me.lblEquip2)
-        Me.GroupBox1.Controls.Add(Me.lblEquip1)
-        Me.GroupBox1.Controls.Add(Me.lblCond)
         Me.GroupBox1.Controls.Add(Me.lblCa)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 59)
         Me.GroupBox1.Name = "GroupBox1"
@@ -249,51 +96,26 @@ Partial Class pf1_2
         Me.GroupBox1.TabIndex = 15
         Me.GroupBox1.TabStop = False
         '
-        'ListBox1
-        '
-        Me.ListBox1.BackColor = System.Drawing.SystemColors.ScrollBar
-        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 18
-        Me.ListBox1.Location = New System.Drawing.Point(159, 18)
-        Me.ListBox1.MinimumSize = New System.Drawing.Size(100, 23)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(126, 22)
-        Me.ListBox1.TabIndex = 16
-        '
         'pf1_2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(314, 305)
-        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.lblCaserne)
         Me.Name = "pf1_2"
-        Me.Text = "Form1"
+        Me.Text = "Départ : Composition"
         Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents lblCaserne As System.Windows.Forms.Label
-    Friend WithEvents lblCa As System.Windows.Forms.Label
-    Friend WithEvents lblCond As System.Windows.Forms.Label
-    Friend WithEvents lblEquip1 As System.Windows.Forms.Label
-    Friend WithEvents lblEquip2 As System.Windows.Forms.Label
-    Friend WithEvents lblEquip3 As System.Windows.Forms.Label
-    Friend WithEvents lblEquip4 As System.Windows.Forms.Label
-    Friend WithEvents lblNomCa As System.Windows.Forms.Label
-    Friend WithEvents lblNomCond As System.Windows.Forms.Label
-    Friend WithEvents lblNomEquip1 As System.Windows.Forms.Label
-    Friend WithEvents lblNomEquip2 As System.Windows.Forms.Label
-    Friend WithEvents lblNomEquip3 As System.Windows.Forms.Label
-    Friend WithEvents lblNomEquip4 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents lblCa As System.Windows.Forms.Label
+    Friend WithEvents lblNomCa As System.Windows.Forms.Label
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 
 End Class
