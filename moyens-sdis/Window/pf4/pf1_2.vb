84171c91778adc62e9f8f63900d24ca41f8fc8d9
AlanJcb <ajacob@estran-brest.education>
2019-03-08 11:10:07 +0100

    bn

--- moyens-sdis/Window/pf4/pf1_2.vb
+++ moyens-sdis/Window/pf4/pf1_2.vb
@@ -5,6 +5,8 @@
     Private Sub pf1_2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 retour:
         nbpomp = InputBox("combien de pompier")
+        Label1.Text = "Nom de caserne"
+        lblCaserne.Text = "Nom du véhicule"
 
 
         Select Case nbpomp
@@ -20,9 +22,14 @@ retour:
                 Pompier6.Hide()
                 GroupBox1.Height = 70
                 GroupBox1.Width = 297
+                Grade1.Text = ""
+                Grade2.Text = ""
+                Pompier1.Text = ""
+                Pompier2.Text = ""
 
-                Button1.Location = New Point(22, 143)
-                Button2.Location = New Point(22, 199)
+                Button1.Location = New Point(27, 137)
+                Button2.Location = New Point(184, 137)
+
 
 
             Case 3
@@ -34,8 +41,19 @@ retour:
                 Pompier6.Hide()
                 GroupBox1.Height = 100
                 GroupBox1.Width = 297
-                Button1.Location = New Point(22, 143)
-                Button2.Location = New Point(22, 199)
+                Grade1.Text = ""
+                Grade2.Text = ""
+                Grade3.Text = ""
+                Pompier1.Text = ""
+                Pompier2.Text = ""
+                Pompier3.Text = ""
+
+
+                Button1.Location = New Point(27, 170)
+                Button2.Location = New Point(184, 170)
+
+
+
             Case 4
                 Grade5.Hide()
                 Grade6.Hide()
@@ -43,36 +61,60 @@ retour:
                 Pompier6.Hide()
                 GroupBox1.Height = 133
                 GroupBox1.Width = 297
-                Button1.Location = New Point(22, 143)
-                Button2.Location = New Point(22, 199)
+                Grade1.Text = ""
+                Grade2.Text = ""
+                Grade3.Text = ""
+                Grade4.Text = ""
+                Pompier1.Text = ""
+                Pompier2.Text = ""
+                Pompier3.Text = ""
+                Pompier4.Text = ""
+
+                Button1.Location = New Point(27, 197)
+                Button2.Location = New Point(184, 197)
             Case 5
                 Grade6.Hide()
                 Pompier6.Hide()
                 GroupBox1.Height = 163
                 GroupBox1.Width = 297
-                Button1.Location = New Point(22, 143)
-                Button2.Location = New Point(22, 199)
+                Grade1.Text = ""
+                Grade2.Text = ""
+                Grade3.Text = ""
+                Grade4.Text = ""
+                Grade5.Text = ""
+                Pompier1.Text = ""
+                Pompier2.Text = ""
+                Pompier3.Text = ""
+                Pompier4.Text = ""
+                Pompier5.Text = ""
+                Button1.Location = New Point(27, 244)
+                Button2.Location = New Point(184, 244)
             Case 6
                 GroupBox1.Height = 188
                 GroupBox1.Width = 297
-                Button1.Location = New Point(22, 143)
-                Button2.Location = New Point(22, 199)
+                Button1.Location = New Point(32, 259)
+                Button2.Location = New Point(184, 259)
+                Grade1.Text = ""
+                Grade2.Text = ""
+                Grade3.Text = ""
+                Grade4.Text = ""
+                Grade5.Text = ""
+                Grade6.Text = ""
+                Pompier1.Text = ""
+                Pompier2.Text = ""
+                Pompier3.Text = ""
+                Pompier4.Text = ""
+                Pompier5.Text = ""
+                Pompier6.Text = ""
 
             Case Else
                 GoTo retour
         End Select
     End Sub
 
+    Private Sub lblCaserne_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblCaserne.Click
+        ' Dim engin As DataTable = Connexion.ORA.Table("SELECT CIS_NOM, ENGIN_ETAT, ENGIN_IMMAT, ENGIN_NOM FROM CASERNE, ENGIN WHERE CASERNE.CIS_ID=ENGIN.CIS_ID AND CIS_NOM='" & Caserne & "';")
+    End Sub
+
     
-    Private Sub lblCa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Grade1.Click
-
-    End Sub
-
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Grade2.Click
-
-    End Sub
-
-    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Grade3.Click
-
-    End Sub
 End Class
