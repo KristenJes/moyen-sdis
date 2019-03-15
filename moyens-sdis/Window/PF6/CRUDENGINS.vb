f768e61db1306e9a38f2d8be9f8c8c3c7dbdc307
unknown <17mleyonco@ESTRANSUP.LOCAL>
2019-03-15 11:08:53 +0100

    b

---
+++ moyens-sdis/Window/PF6/CRUDENGINS.vb
@@ -0,0 +1,89 @@
+﻿
+Class CRUDENGINS
+    'LIAISON AVEC IHM
+    Public Sub afficheIHM(ByVal uneFeuille As Form, ByVal uneListView As ListView)
+        uneFeuille.Text = "ENGINS"
+
+        uneListView.Clear()
+
+        uneListView.Columns.Add("Caserne", 80, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Nom engin", 70, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Imatriculation", 90, HorizontalAlignment.Left)
+        uneListView.Columns.Add("Etat", 120, HorizontalAlignment.Left)
+
+        Dim info(5) As String
+        Dim itm As ListViewItem
+
+        'info(0) = "Ouessant"
+        'info(1) = "VSAV02"
+        'info(2) = "XXX-123-YYY"
+        'info(3) = "OK"
+
+        Dim engin As DataTable = Connexion.ORA.Table("SELECT ENGIN_ID, ENGIN_ETAT, ENGIN_IMMAT, ENGIN_NOM FROM ENGIN")
+        For Each nom As DataRow In engin.Rows
+            info(0) = nom("ENGIN_ID").ToString
+            info(1) = nom("ENGIN_NOM").ToString
+            info(2) = nom("ENGIN_IMMAT").ToString
+            info(3) = nom("ENGIN_ETAT").ToString
+
+            itm = New ListViewItem(info)
+            uneListView.Items.Add(itm)
+        Next
+
+
+
+        ''Dim caserne As DataTable = Connexion.ORA.Table("SELECT CIS_NOM FROM CASERNE")
+        ''For Each nom As DataRow In caserne.Rows
+        ''    info(0) = nom("CIS_NOM").ToString
+
+        ''    itm = New ListViewItem(info)
+        ''    uneListView.Items.Add(itm)
+        ''Next
+
+
+
+
+
+    End Sub
+
+    Public Sub AfficheCaserne(ByVal uneListView As ListView)
+
+
+        Dim info(2) As String
+        Dim itm As ListViewItem
+
+
+        Dim caserne As DataTable = Connexion.ORA.Table("SELECT CIS_NOM FROM CASERNE")
+        For Each nom As DataRow In caserne.Rows
+            info(0) = nom("CIS_NOM").ToString
+
+            itm = New ListViewItem(info)
+            uneListView.Items.Add(itm)
+
+        Next
+    End Sub
+
+    Public Sub ConfirmDelete()
+
+        MessageBox.Show("Êtes-vous sur de vouloir supprimer cet engins ?", "Confirmation de suppression", _
+              MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+    End Sub
+    'Public Sub AfficheTypeEngin(ByVal uneList As ListBox)
+
+    '    Dim info(2) As String
+    '    Dim itm As ListViewItem
+
+
+
+    '    Dim type As DataTable = Connexion.ORA.Table("SELECT TYPE_ENG_NOM FROM TYPE_ENGIN ORDER BY TYPE_ENG_NOM")
+    '    For Each nom As DataRow In type.Rows
+    '        info(0) = nom("TYPE_ENG_NOM").ToString
+
+    '        itm = New ListViewItem(info)
+    '        uneListBox.Items.Add(itm)
+
+    '    Next
+
+    'End Sub
+End Class
---
+++ moyens-sdis/Window/PF6/gestion_engins.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Data.SqlClient
+
+Public Class gestion_engins
+    Dim unEngins As New CRUDENGINS
+
+    Private Sub IHM()
+        unEngins.afficheIHM(Me, lstAffichEngins)
+
+    End Sub
+
+    Private Sub btnSupprimer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSupprimer.Click
+        unEngins.ConfirmDelete()
+
+    End Sub
+    Private Sub btnAjouter_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAjouter.Click
+        gestion_engins_ajout.ShowDialog()
+    End Sub
+
+    Private Sub btnModifier_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModifier.Click
+        gestion_modif.ShowDialog()
+    End Sub
+
+    Private Sub gestion_engins_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        IHM()
+        AfficheCaserne()
+
+        'lstAffichEngins.
+
+    End Sub
+    Public Sub AfficheCaserne()
+        '_________VARIABLE_________
+        Dim info(2) As String
+        Dim type As DataTable = Connexion.ORA.Table("SELECT CIS_ID, CIS_NOM FROM CASERNE")
+        Dim comboSource As New Dictionary(Of String, String)()
+        '__________________________
+
+
+        For Each nom As DataRow In type.Rows
+            info(0) = nom("CIS_ID").ToString
+            info(1) = nom("CIS_NOM").ToString
+
+            comboSource.Add(info(0), info(1))
+            CbCaserne.DataSource = New BindingSource(comboSource, Nothing)
+        Next
+        CbCaserne.DisplayMember = "Value"
+        CbCaserne.ValueMember = "Key"
+    End Sub
+
+    Private Sub lstAffichEngins_ItemCheck(ByVal sender As Object, ByVal e As System.Windows.Forms.ItemCheckEventArgs) Handles lstAffichEngins.ItemCheck
+
+    End Sub
+
+    Private Sub lstAffichEngins_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstAffichEngins.SelectedIndexChanged
+        ' lvw = listview
+        ' Création d'une variable pour une manipulation plus aisée
+        ' SelectedItems est la collection des item sélectionnée ici en l'occurence 
+        ' il n 'y en aura que jamais qu'une.
+        '' -------------------------------------
+        'Vérification qu'il y a bien une ligne sélectionner
+        'Indispensable sinon une erreur apparait car il y a une déselection puis une sélection qui arrive
+        If lstAffichEngins.SelectedItems.Count > 0 Then
+            Dim item As ListViewItem = lstAffichEngins.SelectedItems(0)
+            'récupération de l'index de l'item dans la listview
+            Dim index As Integer = lstAffichEngins.Items.IndexOf(item)
+            ' Récupération des valeurs avec l'utilisation de l'énumération
+            Dim valueColum1 As String = item.SubItems(columnIndex.column1).Text
+            Dim valueColum2 As String = item.SubItems(columnIndex.column2).Text
+
+        End If
+    End Sub
+End Class
