911b584a37e91fd7d4f41ae4138da342121f8d65
Kristen <kjestin@estran-brest.education>
2019-03-22 10:18:47 +0100

    FIN

--- moyens-sdis/Window/PF1/PF1.vb
+++ moyens-sdis/Window/PF1/PF1.vb
@@ -1,15 +1,15 @@
 ﻿Public Class PF1
 
     Dim intervention As Intervention
-    Dim sinistre As New Sinistre(1, "Malaise")
+    Dim sinistre As New Sinistre(3, "Malaise")
     Dim caserneSelected As Caserne
     Dim neededVehicles As New List(Of TypeEngin)
-    Dim selectedEngin As New List(Of Engin)
+    Dim selectedEngin As New Dictionary(Of Engin, Caserne)
     Dim caserneNom As String = "BREST"
 
     Private Sub PF1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-        intervention = New Intervention(2)
+        intervention = New Intervention(4)
 
 
         Dim departTypes As DataTable = Connexion.ORA.Table("SELECT te.TYPE_ENG_ID, te.TYPE_ENG_NOM FROM PREVU, TYPE_ENGIN te WHERE(PREVU.TYPE_ENG_ID = te.TYPE_ENG_ID) AND SIN_ID = " & sinistre.ID)
@@ -28,14 +28,16 @@
                     For Each neededVehicule As TypeEngin In neededVehicles
                         If (caserne.Engins.Count <> 0) Then
                             For Each typeEng As Engin In caserne.getEnginsFromType(neededVehicule)
-
-                                selectedEngin.Add(typeEng)
-
                                 If (neededVehicles.Count = selectedEngin.Count) Then
                                     GoTo end_of_for
                                 End If
-                                For counter = 0 To typeEng.Type.nbPlace
-                                Next
+                                If (InSelected(typeEng.Type)) Then
+                                    GoTo next_of_for
+                                End If
+
+                                selectedEngin.Add(typeEng, caserne)
+
+next_of_for:
                             Next
                         End If
                     Next
@@ -48,7 +50,7 @@
 
 end_of_for:
 
-        For Each engin In selectedEngin
+        For Each engin In selectedEngin.Keys
             Panel1.Controls.Add(Engin_Display(engin))
         Next
     End Sub
@@ -66,11 +68,32 @@ end_of_for:
 
     Public Sub SendEnginsInDepart()
         Dim departID = Connexion.ORA.Champ("SELECT MAX(DEP_ID) FROM DEPART")(0) + 1
-        For Each engin In selectedEngin
-            Connexion.ORA.Execute("INSERT INTO depart (DEP_ID, DEP_DTE_DEPART, DEP_DTE_RETOUR, DEP_DTE_KILOMETRAGE, DEP_COMMENTAIRE, INTERV_ID, ENGIN_ID) VALUES (" & departID & ", sysdate, null, 0, null, " & intervention.ID & ", " & engin.ID & ")")
+        For Each engin In selectedEngin.Keys
+            Connexion.ORA.Execute("INSERT INTO DEPART (DEP_ID, DEP_DTE_DEPART, DEP_DTE_RETOUR, DEP_DTE_KILOMETRAGE, DEP_COMMENTAIRE, INTERV_ID, ENGIN_ID) VALUES ('" & departID & "', sysdate, null, 0, null, '" & intervention.ID & "', '" & engin.ID & "');")
+
+            Dim pompiers As New List(Of Pompier)
+            pompiers = selectedEngin(engin).GetPompierEnService()
+            For counter = 0 To engin.Type.nbPlace
+                Dim pompier As Pompier
+                pompier = pompiers(counter)
+                Connexion.ORA.Execute("INSERT INTO participe (DEP_ID, SP_MATRICULE) VALUES (" & departID & ", '" & pompier.Matricule & "');")
+            Next
+
             departID += 1
         Next
     End Sub
+
+    Public Function InSelected(ByVal type As TypeEngin)
+        Dim inList As Boolean = False
+        For Each engin As Engin In selectedEngin.Keys
+            If (engin.Type.Equals(type)) Then
+                inList = True
+                GoTo end_of_f
+            End If
+        Next
+end_of_f:
+        Return inList
+    End Function
 
     Public Function Engin_Display(ByVal camion)
         Dim group As New GroupBox
@@ -102,7 +125,6 @@ end_of_for:
     End Function
 
     Private Sub btnValider_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnValider.Click
-        MessageBox.Show(selectedEngin.Count)
         SendEnginsInDepart()
     End Sub
 End Class
--- moyens-sdis/Window/PF4/pf1_2.vb
+++ moyens-sdis/Window/PF4/pf1_2.vb
@@ -1,144 +1,31 @@
 ﻿Public Class pf1_2
-    Dim nIntervention As Integer
-    Dim nDepart As Integer
-    Private TypeVeh As String
+
+
 
     Private Sub pf1_2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-retour:
-        TypeVeh = InputBox("combien de pompier")
-        Label1.Text = "Nom de caserne"
-        lblCaserne.Text = "Nom du véhicule"
 
+        'ListBox1.Items.Add("BREST")
+        'ListBox1.Items.Add("OUESSANT")
+        'ListBox1.Items.Add("SAINT RENAN")
+    End Sub
 
-        Select Case TypeVeh
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
 
-            Case "VL"
-                Grade3.Hide()
-                Grade4.Hide()
-                Grade5.Hide()
-                Grade6.Hide()
-                Pompier3.Hide()
-                Pompier4.Hide()
-                Pompier5.Hide()
-                Pompier6.Hide()
-                GroupBox1.Height = 70
-                GroupBox1.Width = 297
-                Grade1.Text = "CA VL"
-                Grade2.Text = "COND VL"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
+    End Sub
 
-                Button1.Location = New Point(27, 137)
-                Button2.Location = New Point(184, 137)
+    Private Sub lblCa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblCa.Click
 
+    End Sub
 
-            Case "VLHR"
-                Grade3.Hide()
-                Grade4.Hide()
-                Grade5.Hide()
-                Grade6.Hide()
-                Pompier3.Hide()
-                Pompier4.Hide()
-                Pompier5.Hide()
-                Pompier6.Hide()
-                GroupBox1.Height = 70
-                GroupBox1.Width = 297
-                Grade1.Text = "CA VLHR"
-                Grade2.Text = "COND VLHR"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
 
-                Button1.Location = New Point(27, 137)
-                Button2.Location = New Point(184, 137)
+    End Sub
 
+    Private Sub lblNomCa_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblNomCa.Click
 
-
-            Case "VTU"
-                Grade4.Hide()
-                Grade5.Hide()
-                Grade6.Hide()
-                Pompier4.Hide()
-                Pompier5.Hide()
-                Pompier6.Hide()
-                GroupBox1.Height = 100
-                GroupBox1.Width = 297
-                Grade1.Text = "CA VTU"
-                Grade2.Text = "COND VTU"
-                Grade3.Text = "EQU VTU"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
-                Pompier3.Text = ""
-
-
-                Button1.Location = New Point(27, 170)
-                Button2.Location = New Point(184, 170)
-
-
-
-            Case "VSAV"
-                Grade4.Hide()
-                Grade5.Hide()
-                Grade6.Hide()
-                Pompier4.Hide()
-                Pompier5.Hide()
-                Pompier6.Hide()
-                GroupBox1.Height = 100
-                GroupBox1.Width = 297
-                Grade1.Text = "CA VSAV"
-                Grade2.Text = "COND VSAV"
-                Grade3.Text = "EQU VSAV"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
-                Pompier3.Text = ""
-
-
-                Button1.Location = New Point(27, 170)
-                Button2.Location = New Point(184, 170)
-            Case "CCF"
-                Grade5.Hide()
-                Grade6.Hide()
-                Pompier5.Hide()
-                Pompier6.Hide()
-                GroupBox1.Height = 133
-                GroupBox1.Width = 297
-                Grade1.Text = "CA CCF"
-                Grade2.Text = "COND CCF"
-                Grade3.Text = "CE CCF"
-                Grade4.Text = "EQU CCF"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
-                Pompier3.Text = ""
-                Pompier4.Text = ""
-
-                Button1.Location = New Point(27, 197)
-                Button2.Location = New Point(184, 197)
-
-            Case "FPT"
-                GroupBox1.Height = 188
-                GroupBox1.Width = 297
-                Button1.Location = New Point(32, 259)
-                Button2.Location = New Point(184, 259)
-                Grade1.Text = "CA FPT"
-                Grade2.Text = "COND FPT"
-                Grade3.Text = "CE FPT"
-                Grade4.Text = "CE FPT"
-                Grade5.Text = "EQU FPT"
-                Grade6.Text = "EQU FPT"
-                Pompier1.Text = ""
-                Pompier2.Text = ""
-                Pompier3.Text = ""
-                Pompier4.Text = ""
-                Pompier5.Text = ""
-                Pompier6.Text = ""
-
-            Case Else
-                GoTo retour
-        End Select
     End Sub
 
     Private Sub lblCaserne_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblCaserne.Click
-        ' Dim engin As DataTable = Connexion.ORA.Table("SELECT CIS_NOM, ENGIN_ETAT, ENGIN_IMMAT, ENGIN_NOM FROM CASERNE, ENGIN WHERE CASERNE.CIS_ID=ENGIN.CIS_ID AND CIS_NOM='" & Caserne & "';")
-    End Sub
 
-    
+    End Sub
 End Class
