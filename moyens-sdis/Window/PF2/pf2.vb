ee29ebfad4cc63a5100d1dd0567ae430a0279f3a
AlanJcb <ajacob@estran-brest.education>
2019-05-23 09:20:48 +0200

    Update pf2.vb

--- moyens-sdis/Window/PF2/pf2.vb
+++ moyens-sdis/Window/PF2/pf2.vb
@@ -272,6 +272,10 @@ retour:
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
         Close()
     End Sub
+
+    Private Sub pf2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
 
 
